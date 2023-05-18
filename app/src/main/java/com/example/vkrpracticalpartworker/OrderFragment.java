24d2a3ca990c0f4b6e004dd6950f76b39d355f9f
NadyaCh <nadya2001chuprova5@gmail.com>
2023-05-18 19:38:33 +0300

    First commit

---
+++ app/src/main/java/com/example/vkrpracticalpartworker/OrderFragment.java
@@ -0,0 +1,37 @@
+package com.example.vkrpracticalpartworker;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OrderFragment extends Fragment {
+
+
+    public OrderFragment() {
+
+    }
+
+    public static OrderFragment newInstance() {
+        OrderFragment fragment = new OrderFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_order, container, false);
+    }
+}
